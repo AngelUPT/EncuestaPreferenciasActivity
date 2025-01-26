faa446ff64becc91674d1cc50fffb966a2be042a
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 13:31:19 -0600

    adding actions

--- app/src/main/java/com/angelcorrea/encuestapreferenciasactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/encuestapreferenciasactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.angelcorrea.encuestapreferenciasactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val colorFavoritoEditText = findViewById<EditText>(R.id.editTextText)
+        val comidaFavoritaEditText = findViewById<EditText>(R.id.editTextText2)
+        val enviarButton = findViewById<Button>(R.id.button)
+
+        enviarButton.setOnClickListener {
+            val colorFavorito = colorFavoritoEditText.text.toString()
+            val comidaFavorita = comidaFavoritaEditText.text.toString()
+
+            if (colorFavorito.isEmpty() || comidaFavorita.isEmpty()) {
+                    Toast.makeText(this, "Completa todos los campos.", Toast.LENGTH_SHORT).show()
+            } else {
+                val mensaje = """
+                    Color Favorito: $colorFavorito
+                    Comida Favorita: $comidaFavorita
+                """.trimIndent()
+
+                Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
